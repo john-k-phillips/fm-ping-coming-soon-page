e5940865cd3358009e3304572e482dc6d937e2ca
John Phillips <johnphillips.dev@gmail.com>
2021-10-18 09:14:14 +0100

    Custom colors and font sizes added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "hsl(223, 87%, 63%)",
+        paleBlue: "hsl(223, 100%, 88%)",
+        lightBlue: "hsl(354, 100%, 66%)",
+        gray: "hsl(0, 0%, 59%)",
+        darkBlue: "hsl(209, 33%, 12%)"
+      }
+    },
   },
   variants: {
     extend: {},
